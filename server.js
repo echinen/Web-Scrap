2d73c55ee81050ab569c04ea3ebb27267af934f6
Eric Chinen <eric_chinen@hotmail.com>
2016-06-16 17:51:23 -0300

    add mais alterações

--- server.js
+++ server.js
@@ -7,40 +7,82 @@ var caminhoSimulado = './Simulados/simENEM2012CienciasHumanas.html';
 var questao;
 var pergunta;
 var urlImagem;
-var respostaA;
-var respostaB;
-var respostaC;
-var respostaD;
-var respostaE;
 var respostaCorreta;
+var count = 0;
+var respostas = [];
+var corretas = [];
 
 fs.readFile(caminhoSimulado,'utf8', (err, data) => {
-    if (err) {
-        throw err;
-    }
+    if (err) {        console.log(err);    }
     
+    //FAZ A LEITURA DO DOCUMENTO
     var $ = cheerio.load(data);
+
+    //LOOP GERAL
+    $('.well').each(function(i, element){
+        var mainLoop = $(this);
+
+        //PEGA O NÚMERO DA QUESTÃO
+        questao = i + 1; 
+
+        //PEGA SOMENTE A PERGUNTA
+        pergunta = mainLoop.children().children().children().parent().next('.span11').text();
+
+        //PEGAS TODAS AS ALTERNATIVAS POR PERGUNTA
+        respostas.push(mainLoop.children().children().children().parent().next().next().children().next().children().children('small').text());
+
+        //PEGA AS URL´s DAS IMAGENS POR PERGUNTA
+        var isUrl = mainLoop.children().children().children().parent().next('.span11').children().children().attr('src');
+
+        if(isUrl != null){urlImagem = isUrl}
+
+        //PEGA TODAS AS RESPOSTAS CORRETAS POR PERGUNTA
+        var isChecked = mainLoop.children().children().children().parent().next('.span12').children().next().children().children();
+
+        corretas.push(isChecked);
+
+        //corretas.push(isChecked.text());
+
+        //console.log(isChecked);
+
+        /*if(somenteRespCorreta.attr('checked')){
+                    //PEGA SOMENTE A RESPOSTA CORRETA
+                    respostaCorreta = somenteRespCorreta.next().text();        
+                }*/
+
+        var metadata = {
+            questao: questao,
+            pergunta: pergunta,
+            //respA: respostaA,
+            //respB: respostaB,
+            //respC: respostaC,
+            //respD: respostaD,
+            //respE: respostaE,
+            respCorreta: respostaCorreta
+        };
+
+        //console.log(metadata);
+    })
+//console.log(respostas[0]);
+//console.log(corretas[0][0].prev.next.next.text);
+//var resp = [];
+
+/*for (var i = 0; i < corretas.length; i++) {
+    //var geral = corretas[i];
     
-    questao = $('#exam-field-714 div .span1 p strong').text();
-    pergunta = $('#exam-field-714 div .span11').text();
-    respostaA = $('#exam-field-714 div .span9 .radio small').first().text();
-    respostaB = $('#exam-field-714 div .span9 .radio small')
-    respostaC = $('#exam-field-714 div .span9 .radio small').text();
-    respostaD = $('#exam-field-714 div .span9 .radio small').text();
-    respostaE = $('#exam-field-714 div .span9 .radio small').last().text();
-
-//var array = Object.getOwnPropertyNames(respostaB)
-
-
-    
-    console.log(respostaB.children().text());    
+    for (var j = 0; j < corretas[i].length; j++) {
+        //var espec = array[j];
         
-    
-    
-    
-    
-    
-    
+        resp.push()
+
+        if(espec.attr('checked')){
+            console.log(espec.text());
+        }
+    }
+}*/
+
+//console.log(t);
+
 });
 /*request(url, function (error, response, body) {
   if (!error) {
